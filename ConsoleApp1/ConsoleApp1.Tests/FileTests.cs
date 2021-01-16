fbed8ce9f42c7ce352f72e1ce9935c8764832dff
Ignot Zarudko <ascendp6n9@gmail.com>
2021-01-16 16:22:09 +0300

    add file constructor tests

---
+++ ConsoleApp1/ConsoleApp1.Tests/FileTests.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp1.Tests.finders;
+using NUnit.Framework;
+
+namespace ConsoleApp1.Tests
+{
+    [TestFixture]
+    public class FileTests
+    {
+        [TestCase("filename.extension", "some strange content, does not matter.")]
+        [TestCase("file-without-extension", "this file has ho extension")]
+        [TestCase(".gitignore", "*/idea")]
+        [TestCase("", "absolutely empty name")]
+        public void Constructor_NameIsCorrect(string name, string content)
+        {
+            File file = new File(name, content);
+            Assert.AreEqual(name, FileInfoFinder.GetFileName(file));
+        }
+
+        [TestCase("filename.extension", "some strange content, does not matter.")]
+        [TestCase(".gitignore", "*/idea")]
+        [TestCase("empty-content-file.txt", "")]
+        public void Constructor_ContentIsCorrect(string name, string content)
+        {
+            File file = new File(name, content);
+            Assert.AreEqual(content, FileInfoFinder.GetFileContent(file));
+        }
+        
+        [TestCase("filename.extension", "some strange content, does not matter.", 19.0)]
+        [TestCase(".gitignore", "*/idea", 3.0)]
+        [TestCase("Multi-line.txt", "some\nlines\ncontent", 9.0)]
+        [TestCase("odd-content-size.txt", "odd content size!", 8.5)]
+        [TestCase("empty-content-file.txt", "", 0.0)]
+        public void Constructor_SizeIsCorrect(string name, string content, double expectedSize)
+        {
+            File file = new File(name, content);
+            Assert.AreEqual(expectedSize, FileInfoFinder.GetFileSize(file));
+        }
+        
+        [TestCase("filename.extension", "some strange content, does not matter.", "extension")]
+        [TestCase("file-without-extension", "this file has ho extension", "")]
+        [TestCase(".gitignore", "#Has 'gitignore' extension\n*/idea", "gitignore")]
+        [TestCase("", "no file extension", "")]
+        public void Constructor_ExtensionIsCorrect(string name, string content, string expectedExtension)
+        {
+            File file = new File(name, content);
+            Assert.AreEqual(expectedExtension, FileInfoFinder.GetFileExtension(file));
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1.Tests/finders/FileInfoFinder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp1.Tests.finders
+{
+    public static class FileInfoFinder
+    {
+        public static string GetFileName(File file)
+        {
+            Type fileType = file.GetType();
+            FieldInfo fieldInfo = typeof(File).GetField("filename", BindingFlags.Instance | BindingFlags.NonPublic);
+            return (string)fieldInfo?.GetValue(file);
+        }
+
+        public static string GetFileContent(File file)
+        {
+            Type fileType = file.GetType();
+            FieldInfo fieldInfo = typeof(File).GetField("content", BindingFlags.Instance | BindingFlags.NonPublic);
+            return (string)fieldInfo?.GetValue(file);
+        }
+
+        public static string GetFileExtension(File file)
+        {
+            Type fileType = file.GetType();
+            FieldInfo fieldInfo = typeof(File).GetField("extension", BindingFlags.Instance | BindingFlags.NonPublic);
+            return (string)fieldInfo?.GetValue(file);
+        }
+
+        public static double GetFileSize(File file)
+        {
+            Type fileType = file.GetType();
+            FieldInfo fieldInfo = typeof(File).GetField("size", BindingFlags.Instance | BindingFlags.NonPublic);
+            return (double)fieldInfo.GetValue(file);
+        }
+    }
+}
