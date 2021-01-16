9e3bb0a7db63de2b44b8bfd455ef1ee05bcac305
Ignot Zarudko <ascendp6n9@gmail.com>
2021-01-16 20:45:53 +0300

    update FileStorageTests

--- ConsoleApp1/ConsoleApp1.Tests/FileStorageTests.cs
+++ ConsoleApp1/ConsoleApp1.Tests/FileStorageTests.cs
@@ -1,4 +1,7 @@
-﻿using ConsoleApp1.Tests.finders;
+﻿using System.Collections.Generic;
+using ConsoleApp1.Tests.finders;
+using FileSystem.exception;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using NUnit.Framework;
 
 namespace ConsoleApp1.Tests
@@ -12,7 +15,7 @@ namespace ConsoleApp1.Tests
         public static void Constructor_MaxSizeIsCorrect(int size, double maxSizeExpected)
         {
             FileStorage fileStorage = new FileStorage(size);
-            Assert.AreEqual(maxSizeExpected, FileStorageInfoFinder.GetFileStorageMaxSize(fileStorage));
+            Assert.AreEqual(maxSizeExpected, FileStorageInfoFinder.GetFileStorageMaxSize(fileStorage), $"Wrong max size, expected {maxSizeExpected}");
         }
         
         [TestCase(123, 223.0)]
@@ -21,7 +24,75 @@ namespace ConsoleApp1.Tests
         public static void Constructor_AvailableSizeIsCorrect(int size, double availableSizeExpected)
         {
             FileStorage fileStorage = new FileStorage(size);
-            Assert.AreEqual(availableSizeExpected, FileStorageInfoFinder.GetFileStorageAvailableSize(fileStorage));
+            Assert.AreEqual(availableSizeExpected, FileStorageInfoFinder.GetFileStorageAvailableSize(fileStorage), $"Wrong max size, expected {availableSizeExpected}");
+        }
+
+        private static IEnumerable<TestCaseData> WriteTestCaseData
+        {
+            get
+            {
+                yield return new TestCaseData(new FileStorage(10), new File("new-file-to-empty.md", "kek"), true);
+                
+                FileStorage fileStorage = new FileStorage();
+                fileStorage.write(new File("file.txt",
+                    "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"));
+                yield return new TestCaseData(fileStorage, new File("new-file-to-not-empty.md", "kek"), true);
+                
+                yield return new TestCaseData(new FileStorage(), new File("new-file-to-empty.md", "too big for this storage!too big for this storage!too big for this storage!too big for this storage!too big for this storage!too big for this storage!too big for this storage!too big for this storage!"), false);
+                
+                fileStorage = new FileStorage();
+                fileStorage.write(new File("file.txt",
+                    "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"));
+                yield return new TestCaseData(fileStorage, new File("new-file-to-not-empty.md", "too big for this storage!too big for this storage!too big for this storage!too big for this storage!too big for this storage!"), false);
+
+            }
+        }
+        
+        [TestCaseSource(typeof(FileStorageTests),nameof(WriteTestCaseData))]
+        public static void Write_ReturnsCorrectResult(FileStorage storage, File file, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, storage.write(file), $"something went wrong while writing file \'{file.getFilename()}\' (expected {expectedResult}, but found {!expectedResult})");
+        }
+
+        private static IEnumerable<TestCaseData> WriteThrowsExceptionData
+        {
+            get
+            {
+                FileStorage fileStorage = new FileStorage();
+                fileStorage.write(new File("file.txt",
+                    "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"));
+                yield return new TestCaseData(fileStorage, new File("file.txt", "it doesn't matter what inside"));
+            }
+        }
+        
+        [TestCaseSource(typeof(FileStorageTests),nameof(WriteThrowsExceptionData))]
+        public static void Write_ThrowsFileNameAlreadyExistsException(FileStorage storage, File file)
+        {
+            Assert.Throws<FileNameAlreadyExistsException>(
+                () => storage.write(file), $"{file.getFilename()} was added, but it should not have"
+                );
+        }
+
+        private static IEnumerable<TestCaseData> IsExistsData
+        {
+            get
+            {
+                FileStorage fileStorage = new FileStorage();
+                yield return new TestCaseData(fileStorage, "full-name.txt", false);
+                
+                fileStorage = new FileStorage();
+                fileStorage.write(new File("full-name.txt", "content"));
+                yield return new TestCaseData(fileStorage, "full-name.txt", true);
+                yield return new TestCaseData(fileStorage, "full-name", true);
+                yield return new TestCaseData(fileStorage, "name", true);
+                yield return new TestCaseData(fileStorage, "some-name.txt", false);
+            }
+        }
+
+        [TestCaseSource(typeof(FileStorageTests), nameof(IsExistsData))]
+        public static void IsExists_ReturnsCorrectResult(FileStorage fileStorage, string filename, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, fileStorage.isExists(filename), $"Got error searching {filename}");
         }
         
         
