443a6ac99343b6d9075c19d1374b744b41f0f5d2
Ignot Zarudko <ascendp6n9@gmail.com>
2021-01-21 16:23:48 +0300

    add TestConfig, which is parsed from settings.json

---
+++ HelloWebdriver/HelloWebdriver.Tests/TestConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HelloWebdriver.Tests
+{
+    public class TestConfig
+    {
+        public string Browser { get; set; }
+        public string StartUrl { get; set; }
+        public string UserLogin { get; set; }
+        public string UserPassword { get; set; }
+        
+        public string Username { get; set; }
+        
+        public string UserEmail { get; set; }
+
+    }
+}
